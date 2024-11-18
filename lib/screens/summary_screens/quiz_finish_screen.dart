9800c1fc1990724c534bdfd9fd3de13ba42efd9e
Daniel Kim <dwkim0101@naver.com>
2024-11-18 22:54:42 +0900

    ✨ Quiz finish screen created

--- lib/screens/summary_screens/quiz_finish_screen.dart
+++ lib/screens/summary_screens/quiz_finish_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:study_helper/screens/summary_screens/quiz_screen.dart';
 import 'package:study_helper/theme/theme_colors.dart';
 
 class QuizFinishScreen extends StatefulWidget {
@@ -19,10 +21,107 @@ class _QuizFinishScreenState extends State<QuizFinishScreen> {
           style: TextStyle(fontSize: 18),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 30),
-        child: Column(
-          children: [],
+      body: Padding(
+        padding:
+            const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 30),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "11월 18일 운영체제",
+                style: TextStyle(
+                  fontSize: 32,
+                  color: colorBottomBarDefault,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Text(
+                "퀴즈결과",
+                style: TextStyle(
+                  fontSize: 32,
+                  color: colorBottomBarDefault,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Gap(40),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.green, shape: BoxShape.circle),
+                    width: 170,
+                    height: 170,
+                  ),
+                  const Text(
+                    "5/6",
+                    style: TextStyle(
+                      fontSize: 64,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(40),
+              Card(
+                color: colorBottomBarDefault,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const QuizScreen(),
+                      ),
+                    );
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "퀴즈 다시풀기",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: colorBottomBarDefault,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "돌아가기",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
